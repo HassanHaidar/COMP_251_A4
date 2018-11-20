7f8060b71f034ba4ba18df1da8f5117f8111beda
Hassan Haidar <hassan.haidar@mail.mcgill.ca>
2018-11-19 22:08:53 -0500

    implement naive and karatsuba multiplication

--- src/Multiply.java
+++ src/Multiply.java
@@ -12,17 +12,70 @@ public class Multiply{
     public static int[] naive(int size, int x, int y) {
 
         // YOUR CODE GOES HERE  (Note: Change return statement)
-        
-        return null;
+    	
+    	int[] result = new int[2]; //array to store our calculations
+    	
+    	// base case
+    	if (size == 1){
+    		result[0] = x * y;
+    		result[1] += 1; // increase cost by one
+    		return result;
+    	}
+    	else{
+    		int m = (int)Math.ceil(size/2.0);	
+    		
+    		int a = (int)Math.floor(x / Math.pow(2, m));
+    		int b = x % ((int)Math.pow(2, m));
+    		
+    		int c = (int)Math.floor(y / Math.pow(2, m));
+    		int d = y % ((int)Math.pow(2, m));
+    		
+    		int[] e = naive(m, a, c);
+    		int[] f = naive(m, b, d);
+    		int[] g = naive(m, b, c);
+    		int[] h = naive(m, a, d);
+    		
+    		result[0] = (int)( Math.pow(2, 2*m) * e[0] + Math.pow(2, m)*(g[0] + h[0]) + f[0]);
+    		result[1] += (e[1] + f[1] + g[1] + h[1] +  3*size) ;
+    		
+    		return result;
+    				
+    	}
         
     }
 
     public static int[] karatsuba(int size, int x, int y) {
         
         // YOUR CODE GOES HERE  (Note: Change return statement)
-        
-        return null;
-        
+    	
+    	int[] result = new int[2]; //array to store our calculations
+    	
+    	if (size == 1){
+    		result[0] = x * y;
+    		result[1] += 1; // increase cost by one
+    		return result;
+    	}
+    	
+    	else{
+    		int m = (int)Math.ceil(size/2.0);	
+    		
+    		int a = (int)Math.floor(x / Math.pow(2, m));
+    		int b = x % ((int)Math.pow(2, m));
+    		
+    		int c = (int)Math.floor(y / Math.pow(2, m));
+    		int d = y % ((int)Math.pow(2, m));
+    		
+    		int[] e = karatsuba(m, a, c);
+    		int[] f = karatsuba(m, b, d);
+    		int[] g = karatsuba(m, a-b, c-d);
+    		
+    		result[0] = (int) (Math.pow(2,2*m) * e[0] + Math.pow(2, m) * (e[0] + f[0] - g[0]) + f[0]);    	
+    		result[1] = (e[1] + f[1] + g[1] + 6 * size);
+    		
+    		return result;
+    		
+    	}
+              
     }
     
     public static void main(String[] args){
@@ -41,7 +94,7 @@ public class Multiply{
             
                     if (resNaive[0] != resKaratsuba[0]) {
                         throw new Exception("Return values do not match! (x=" + x + "; y=" + y + "; Naive=" + resNaive[0] + "; Karatsuba=" + resKaratsuba[0] + ")");
-                    }
+                   }
                     
                     if (resNaive[0] != (x*y)) {
                         int myproduct = x*y;
